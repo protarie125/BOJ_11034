ede73e685b980607b0792bd142e4924470e065c2
protarie125 <proarie@gmail.com>
2021-11-24 21:01:45 +0900

    Passed

--- BOJ_11034/src/main.cpp
+++ BOJ_11034/src/main.cpp
@@ -6,5 +6,19 @@ int main() {
     ios::sync_with_stdio(false);
     std::cin.tie(nullptr);
 
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    while (cin) {
+        auto ab = b - a;
+        auto bc = c - b;
+
+        auto jump = max(ab - 1, bc - 1);
+        cout << jump << '\n';
+
+        // next
+        cin >> a >> b >> c;
+    }
+
     return 0;
 }
